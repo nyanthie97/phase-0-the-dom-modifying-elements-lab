4a1683fc2ff66ddb070415f4c935798a1887612b
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-14 22:32:28 +0300

    commit tree 309e32fef1e43cc6f25aba7bc264f4548d81095b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = ("victory");
+newHeader.textContent = "YOUR-NAME is the champion";
